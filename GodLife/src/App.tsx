b61686123fe6364e04204e514accfd7f4604875b
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-11-17 01:54:00 +0900

    feat: 초기 환경 설정
    - typescript
    - vite
    - tailwind v3

---
+++ GodLife/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center border bg-red-500 text-3xl text-white">
+      TAILWIND TEST 🔥
+    </div>
+  );
+}
+
+export default App;
